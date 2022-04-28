60649e57871288e17843a12c0471b9b3277c1a42
Damon German <damon.german@hyland.com>
2022-04-28 11:37:08 -0500

    Add enemy

---
+++ Space Shooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed = 4f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(Vector3.down * (_speed * Time.deltaTime));
+        if (transform.position.y < -5f)
+        {
+            transform.position = new Vector3(Random.Range(-8f, 8f), 7, 0);
+        }
+    }
+}
