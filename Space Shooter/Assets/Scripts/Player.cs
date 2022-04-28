eb1bb2e77896a08bf179cac2d03399c825b636dc
Damon German <damon.german@hyland.com>
2022-04-28 09:56:45 -0500

    some cleanup

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -3,24 +3,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 3.5f;
+    [SerializeField] private GameObject _laser;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         transform.position = Vector3.zero;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        }
     }
 
     private void CalculateMovement()
     {
         var horizontalInput = Input.GetAxis("Horizontal");
         var verticalInput = Input.GetAxis("Vertical");
-
 
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * (_speed * Time.deltaTime));
 
