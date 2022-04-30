0fd5ca2daf12af4e15886487660fecd05cf5a6f4
Damon German <damon.german@hyland.com>
2022-04-30 16:33:25 -0500

    made some changes

--- Space Shooter/Assets/Scripts/Enemy.cs
+++ Space Shooter/Assets/Scripts/Enemy.cs
@@ -18,4 +18,18 @@ public class Enemy : MonoBehaviour
             transform.position = new Vector3(Random.Range(-8f, 8f), 7, 0);
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+
+        if (other.tag == "Lazer")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
 }
--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speed = 3.5f;
     [SerializeField] private GameObject _laserPrefab;
     [SerializeField] private float _fireRate = 0.5f;
+    [SerializeField] private int _lives = 3;
     private float _canFire = -1f;
 
     private void Start()
@@ -53,5 +54,15 @@ public class Player : MonoBehaviour
     {
         _canFire = Time.time + _fireRate;
         Instantiate(_laserPrefab, new Vector3(transform.position.x, transform.position.y + 0.8f, 0), Quaternion.identity);
+    }
+
+    public void Damage()
+    {
+        _lives--;
+
+        if (_lives < 1)
+        {
+            Destroy(gameObject);
+        }
     }
 }
